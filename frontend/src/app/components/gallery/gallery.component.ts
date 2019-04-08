0cabd3ca5c6a2e4d87a71128f7731826a9e69f32
celina <celina.urwank@outlook.com>
2019-04-08 13:40:43 +0200

    Too large file fix attempt

--- frontend/src/app/components/gallery/gallery.component.ts
+++ frontend/src/app/components/gallery/gallery.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/core/modal.service';
+
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private modalService: ModalService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  openModal(id: string) {
+    this.modalService.open(id);
+  }
+
+  closeModal(id: string) {
+    this.modalService.close(id);
+  }
+
+
+  // uploader
+  isHovering: boolean;
+
+  files: File[] = [];
+
+  toggleHover(event: boolean) {
+    this.isHovering = event;
+  }
+
+  onDrop(files: FileList) {
+    for (let i = 0; i < files.length; i++) {
+      this.files.push(files.item(i));
+    }
   }
 
 }
+
